3230c441b086cf26406fc78b434b6c930f55b987
Dmitriy Ryzhov <superdiman_1994@mail.ru>
2023-04-20 19:43:54 +0800

    Удалил часть заголовка

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
